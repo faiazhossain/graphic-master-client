f32436175ab1329982650c3b5385d650d90e0bdf
Faiaz Hossain <faiazhossain10@gmail.com>
2022-10-25 10:50:49 +0600

    initialized project

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAb93lazSDlNjmBbc0FvfC4H0TCWJ1DGpM",
+  authDomain: "graphic-master-148cd.firebaseapp.com",
+  projectId: "graphic-master-148cd",
+  storageBucket: "graphic-master-148cd.appspot.com",
+  messagingSenderId: "1091698746086",
+  appId: "1:1091698746086:web:3c84b53e701c9e24d75127",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
